9fb339d4e329abc0047dadaf9ee90bc62181928c
Javier Sanchez <jsanchez0508@hotmail.com>
2021-10-10 17:22:55 -0500

    Cambios minimos

--- ServicioMensajeria/src/Model/ServicioDAO.java
+++ ServicioMensajeria/src/Model/ServicioDAO.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
-/**
- *
- * @author Usuario
- */
+import Util.SMException;
+
 public class ServicioDAO {
-    
+    public void registrarServicio() throws SMException{
+        
+    }
 }
